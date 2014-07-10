b59e0fe0f5372e844239ef0949fe41a6ae3c389c
Barney Hanlon <bhanlon@inviqa.com>
2014-07-10 15:08:09 +0100

    Created array of ips in separate file

---
+++ iptables/ips.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+declare -A WHITELIST
+
+WHITELIST['frontdoor']='78.109.163.185'   # Frontdoor
+WHITELIST['frontdoor1']='184.72.232.234'  # Frontdoor 1
+WHITELIST['frontdoor2']='23.21.72.114'    # Frontdoor 2
+WHITELIST['london']='141.0.34.138'        # London Office
+WHITELIST['london_dsl']='62.49.114.11'    # London Office (DSL backup)
+WHITELIST['sheffield']='81.23.63.12'      # Sheffield Office
+WHITELIST['leeds']='88.211.104.155'       # Leeds Office
+WHITELIST['manchester']='151.237.238.126' # Manchester Office
+WHITELIST['scothost']='88.208.220.171'    # ScotHost VPN
+WHITELIST['liverpool']='37.191.108.74'	  # Liverpool Office
+WHITELIST['kaunas']='87.247.82.92'        # Kaunas Office
